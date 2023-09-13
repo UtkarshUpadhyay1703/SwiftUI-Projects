652efd0cb40073198e1d3a3e95291a5270d3c280
UtkarshUpadhyay1703 <utkarsh1703@gmail.com>
2023-09-13 19:28:21 +0530

    on click toolbar icon badge changes to 5

---
+++ Safari Extensions/Toolbar/Toolbar Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  Toolbar Extension
+//
+//  Created by admin on 9/13/23.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
